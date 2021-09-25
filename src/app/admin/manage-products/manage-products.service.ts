e3162c194382a42975dcba37031523f0880019ac
Viktor <smirnovv00@gmail.com>
2021-09-25 22:47:29 +0300

    task-5

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Injector } from '@angular/core';
 import { EMPTY, Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map } from 'rxjs/operators';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -32,10 +32,12 @@ export class ManageProductsService extends ApiService {
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
 
-    return this.http.get<string>(url, {
+    return this.http.get<{ url: string }>(url, {
       params: {
         name: fileName,
       },
-    });
+    }).pipe(
+      map(({ url }) => url),
+    );
   }
 }
